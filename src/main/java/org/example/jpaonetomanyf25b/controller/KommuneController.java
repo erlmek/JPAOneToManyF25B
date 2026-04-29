2b49fe1f6f9386f430d4ac6392f83645cec2c2bd
erlmek <erlm@ek.dk>
2026-04-29 10:22:42 +0200

    kommuner starts with

--- src/main/java/org/example/jpaonetomanyf25b/controller/KommuneController.java
+++ src/main/java/org/example/jpaonetomanyf25b/controller/KommuneController.java
@@ -3,6 +3,7 @@ package org.example.jpaonetomanyf25b.controller;
 import org.example.jpaonetomanyf25b.model.Kommune;
 import org.example.jpaonetomanyf25b.model.Region;
 import org.example.jpaonetomanyf25b.repository.KommuneRepository;
+import org.example.jpaonetomanyf25b.service.ApiServiceGetKommuner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,15 @@ public class KommuneController {
 
     @Autowired
     KommuneRepository kommuneRepository;
+
+    @Autowired
+    ApiServiceGetKommuner apiServiceGetKommuner;
+
+    @GetMapping("/kommunestart/{letter}")
+    public ResponseEntity<List<Kommune>> getKommuneStartsWith(@PathVariable String letter) {
+        List<Kommune> lst = apiServiceGetKommuner.getKommunerStartsWith(letter.charAt(0));
+        return ResponseEntity.ok(lst);
+    }
 
     @GetMapping("kommuner")
     public List<Kommune> getKommuner(){
